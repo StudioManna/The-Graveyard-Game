9fc4d695d39fd3eb3daf308e1543d1825fe0cf11
Zach Hoskins <zach@zachos.tech>
2023-07-19 15:35:25 -0500

    0.1.3-d

--- build.bat
+++ build.bat
@@ -41,7 +41,7 @@ echo Previous build removed
 echo:
 
 echo Starting WebGL Build. May take a few minutes.
-cd %unityDir%
+cd "%unityDir%"
 cmd /C ".\Unity.exe -quit -batchmode -nographics -projectProject %projectDirectory% -executeMethod WebGLBuilder.build"
 echo Build Complete
 echo:
