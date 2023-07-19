af698bfee8e3d1e30370996e484c29ec409ee763
Zach Hoskins <zach@zachos.tech>
2023-07-19 15:25:08 -0500

    0.1.3

---
+++ build.bat
@@ -0,0 +1,55 @@
+@echo off
+set unityDir=C:\Program Files\Unity\Hub\Editor\2022.3.4f1\Editor\Unity.exe
+set projectDirectory=D:\dev\gamedev\GDFG Game Jam July 2023
+
+IF NOT EXIST "%unityDir%" (
+    echo Unity Not Installed, or incorrect unityDir variable
+    pause
+    exit /b
+)
+
+IF NOT EXIST "%projectDirectory%" (
+    echo Incorrect project directory. Check projectDirectory variable, and be sure not to use quotation marks.
+    pause
+    exit /b
+)
+
+IF NOT EXIST "%projectDirectory%\Build\version.txt" (
+    echo No version file. Add version.txt to the Build folder with the correct Semantic Versioning number.
+    pause
+    exit /b
+)
+
+for /f "delims=" %%i in ('git log -1 --format^="%%s"') do set "commit_message=%%i"
+
+set /p version=<"%projectDirectory%\Build\version.txt"
+
+if %commit_message%==%version% (
+    echo Version is the same as the last git commit. Please use Semantic Versioning
+    pause
+    exit /b
+)
+
+cd %projectDirectory%
+
+echo:
+
+echo Removing previous build folders
+rmdir "%projectDirectory%\Build\Build" /s /q
+rmdir "%projectDirectory%\Build\TemplateData" /s /q
+del "%projectDirectory%\Build\index.html"
+echo Previous build removed
+
+echo:
+
+echo Starting WebGL Build. May take a few minutes.
+cd %unityDir%
+cmd /C ".\Unity.exe -quit -batchmode -nographics -executeMethod WebGLBuilder.build"
+echo Build Complete
+echo:
+echo Committing v%version% to GitHub
+cd "%projectDirectory%\Build"
+cmd /C "git add . & git commit -m %version% & git push"
+echo Commit successful
+pause
+exit /b 0
